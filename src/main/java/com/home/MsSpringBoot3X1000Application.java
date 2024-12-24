d237f8e1efe61ac92e10b0722adb83174b6d945e
rahulbaliyan@gmail.com <rahulbaliyan@gmail.com>
2024-12-24 18:36:24 +0530

    First Commit

---
+++ src/main/java/com/home/MsSpringBoot3X1000Application.java
@@ -0,0 +1,13 @@
+package com.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSpringBoot3X1000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSpringBoot3X1000Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/MsSpringBoot3X1000ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSpringBoot3X1000ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
