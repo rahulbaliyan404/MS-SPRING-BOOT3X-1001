6e805c87b9ae2a5c33f5ec80988408361706c68d
rahulbaliyan@gmail.com <rahulbaliyan@gmail.com>
2024-12-25 08:29:33 +0530

    second commit

--- src/main/java/com/home/MsSpringBoot3X1001Application.java
+++ src/main/java/com/home/MsSpringBoot3X1001Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsSpringBoot3X1000Application {
+public class MsSpringBoot3X1001Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsSpringBoot3X1000Application.class, args);
+		SpringApplication.run(MsSpringBoot3X1001Application.class, args);
 	}
 
 }
---
+++ src/main/java/com/home/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.home.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+@RequestMapping(value = "/BOOT3X1001")
+public interface UserController {
+    @GetMapping(value = "/users")
+    public List<String> userList();
+
+    @GetMapping(value = "/user/{userId}")
+    public String getUser(@PathVariable("userId") int id);
+}
